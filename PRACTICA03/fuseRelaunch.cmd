d7aa577fe49f3de633f5e568239a9b9e47de4edf
Angel N Ramírez <iangelmx@hotmail.com>
2018-03-14 21:23:40 -0600

    Se simula satisfactoriamente XD

---
+++ PRACTICA03/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "secureip" -o "C:/Users/iAngelMx/Documents/ESCOM/ArqComp/arqCompVHDL/PRACTICA03/PRACTICA03_isim_beh.exe" -prj "C:/Users/iAngelMx/Documents/ESCOM/ArqComp/arqCompVHDL/PRACTICA03/PRACTICA03_beh.prj" "work.PRACTICA03" 

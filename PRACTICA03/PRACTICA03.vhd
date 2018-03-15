19553f6c3dcb9141eb2e288a745cb3955ae44d60
Angel N Ramírez <iangelmx@hotmail.com>
2018-03-14 21:19:09 -0600

    Se sintetiza de manera correcta la práctica

--- PRACTICA03/PRACTICA03.vhd
+++ PRACTICA03/PRACTICA03.vhd
@@ -22,8 +22,8 @@ signal carries, R_aux : STD_LOGIC_VECTOR(3 downto 0);
 signal oper_aux : STD_LOGIC_VECTOR(1 downto 0);
 
 begin
-	sel_a<=operacion(3);
-	sel_b<=operacion(2);
+	sel_aAux<=operacion(3);
+	sel_bAux<=operacion(2);
 	oper_aux<=operacion(1 downto 0);
 	ALU01: ALU_1BIT port map(a=>A(0), b=>B(0), sel_a=>sel_aAux, sel_b=>sel_bAux, cin=>sel_bAux, operacion=>oper_aux, cout=>carries(0), resul=>R_aux(0));
 	ALU02: ALU_1BIT port map(a=>A(1), b=>B(1), sel_a=>sel_aAux, sel_b=>sel_bAux, cin=>carries(0), operacion=>oper_aux, cout=>carries(1), resul=>R_aux(1));
@@ -33,7 +33,7 @@ begin
 	Z<= (R_aux(0) nor R_aux(1)) nor (R_aux(2) nor R_aux(3));
 	
 	R<=R_aux;
-	C<= carries(3);
+	Cout<= carries(3);
 	
 
 end Behavioral;

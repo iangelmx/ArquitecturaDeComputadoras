7c1e85652440dd943e3200bec4b465087dc15c43
iangelmx <iangelmx@hotmail.com>
2018-04-15 23:49:00 -0500

    Se hacen correcciones a practica MEM PROG

--- MEMORIA_PROGRAMA/MEMORIA_PROGRAMA.vhd
+++ MEMORIA_PROGRAMA/MEMORIA_PROGRAMA.vhd
@@ -37,7 +37,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use iEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity memProg is
+entity MEMORIA_PROGRAMA is
 	generic
 	(
 		palabra : integer := 25;
@@ -48,32 +48,31 @@ entity memProg is
 		dir		: in 	std_logic_vector(busT-1 downto 0);
 		data		: out	std_logic_vector(palabra-1 downto 0)
 	);
-end memProg;
+end MEMORIA_PROGRAMA;
 
-architecture Behavioral of memProg is
+architecture Behavioral of MEMORIA_PROGRAMA is
 
   constant LI		: std_logic_vector(4 downto 0)	:= "00001";
   
-  constant R0		: std_logic_vector(3 downto 0)	:= "0000";
-  constant R1		: std_logic_vector(3 downto 0)	:= "0001";
+  constant R0		: std_logic_vector(3 downto 0)	:= x"0";
+  constant R1		: std_logic_vector(3 downto 0)	:= x"1";
 
 
   constant ADD	: std_logic_vector(4 downto 0)	:= "00000";
   constant SWI	: std_logic_vector(4 downto 0)	:= "00011";
-
-  constant ADDI	: std_logic_vector(4 downto 0)	:= "00101";
   constant B		: std_logic_vector(4 downto 0)	:= "10011";
 
 type arr is array (0 to ((2**busT)-1)) of std_logic_vector((palabra-1) downto 0);
 
-constant banco: arr :=	(
-                        LI&R0&x"0001",
-                        LI&R1&x"0007",
-                        ADD&R1&R1&R0&"0000"&"00",
-                        SWI&R1&x"0005",
-                        B&"0000"&x"0002",
-								others => (others => '0') );
+constant banco: arr :=(
 
+                        LI&R0&x"0005",							--"00001 0000 0000 0000 0000 0101",
+                        LI&R1&x"0007",							--"00001 0001 0000 0000 0000 1011",
+								ADD&R1&R1&R0&"0000"&"0000",			--"00000 0001 0001 0000 0000 0000",
+                        SWI&R1&x"0005",						--"00011 0001 0000 0000 0000 0101",
+                        B&"0000"&x"0002",						--"1001100000000000000000010",
+                        
+								others => (others => '0') );
 begin
 
 	data <= banco(conv_integer(dir));

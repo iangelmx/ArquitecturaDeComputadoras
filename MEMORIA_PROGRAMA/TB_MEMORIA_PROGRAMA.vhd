b71f6e60ce90ac92f213f63ac70d570111a71340
yaelcruz97 <ayhc97@hotmail.com>
2018-04-16 12:48:21 -0500

    P5_pendiente  P6_terminada

--- MEMORIA_PROGRAMA/TB_MEMORIA_PROGRAMA.vhd
+++ MEMORIA_PROGRAMA/TB_MEMORIA_PROGRAMA.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer:
 --
--- Create Date:   00:04:05 04/16/2018
+-- Create Date:   11:37:22 04/16/2018
 -- Design Name:   
 -- Module Name:   C:/Users/ayhc9/arqCompVHDL/MEMORIA_PROGRAMA/TB_MEMORIA_PROGRAMA.vhd
 -- Project Name:  MEMORIA_PROGRAMA
@@ -26,6 +26,9 @@
 -- simulation model.
 --------------------------------------------------------------------------------
 LIBRARY ieee;
+LIBRARY STD;
+USE STD.TEXTIO.ALL;
+USE ieee.std_logic_TEXTIO.ALL;
 USE ieee.std_logic_1164.ALL;
  
 -- Uncomment the following library declaration if using
@@ -40,18 +43,18 @@ ARCHITECTURE behavior OF TB_MEMORIA_PROGRAMA IS
     -- Component Declaration for the Unit Under Test (UUT)
  
     COMPONENT MEMORIA_PROGRAMA
-    PORT(
-         dir : IN  std_logic_vector(15 downto 0);
-         data : OUT  std_logic_vector(24 downto 0)
+	 PORT(
+         PC : IN  std_logic_vector(15 downto 0);
+         instruccion : OUT  std_logic_vector(24 downto 0)
         );
     END COMPONENT;
     
 
    --Inputs
-   signal dir : std_logic_vector(15 downto 0) := (others => '0');
+   signal PC : std_logic_vector(15 downto 0) := (others => '0');
 
  	--Outputs
-   signal data : std_logic_vector(24 downto 0);
+   signal instruccion : std_logic_vector(24 downto 0);
    -- No clocks detected in port list. Replace <clock> below with 
    -- appropriate port name 
  
@@ -61,8 +64,8 @@ BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
    uut: MEMORIA_PROGRAMA PORT MAP (
-          dir => dir,
-          data => data
+          PC => PC,
+          instruccion => instruccion
         );
 
    -- Clock process definitions
@@ -76,11 +79,65 @@ BEGIN
  
 
    -- Stimulus process
-   stim_proc: process
-   begin		
+   stim_proc: process		
       -- hold reset state for 100 ns.
-      wait for 100 ns;	
+--   wait for 100 ns;	
 
+	file ARCH_RES : TEXT;																					
+	variable LINEA_RES : line;
+	VARIABLE var_instruccion : STD_LOGIC_VECTOR(24 DOWNTO 0);
+	
+	file ARCH_VEC : TEXT;
+	variable LINEA_VEC : line;
+	VARIABLE var_pc : STD_LOGIC_VECTOR(15 DOWNTO 0);
+	VARIABLE CADENA : STRING(1 TO 6);
+   begin		
+		file_open(ARCH_VEC, "IN.TXT", READ_MODE); 	
+		file_open(ARCH_RES, "OUT.TXT", WRITE_MODE); 	
+
+		--ENCABEZADPS DE OUT.TXT
+		CADENA := "  A   ";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);
+		CADENA := "OPCODE";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);
+		CADENA := "19-16 ";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);
+		CADENA := "15-12 ";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);
+		CADENA := "11-8  ";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);
+		CADENA := "7-4   ";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);
+		CADENA := "3-0   ";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);
+		writeline(ARCH_RES,LINEA_RES);-- escribe la linea en el archivo
+
+		WAIT FOR 10 NS;
+		FOR I IN 0 TO 6 LOOP
+			readline(ARCH_VEC,LINEA_VEC); -- lee una linea completa
+
+			Hread(LINEA_VEC, var_pc);
+			PC <= var_pc;
+			
+--			WAIT UNTIL RISING_EDGE(CLK);	--ESPERO AL FLANCO DE SUBIDA 
+
+			var_instruccion := instruccion;	
+			Hwrite(LINEA_RES, var_pc, right, 5);	--ESCRIBE EL CAMPO PC
+			write(LINEA_RES, var_instruccion(24 downto 20), 	right, 9);
+			write(LINEA_RES, var_instruccion(19 downto 16), 	right, 6);
+			write(LINEA_RES, var_instruccion(15 downto 12), 	right, 6);
+			write(LINEA_RES, var_instruccion(11 downto 8), 	right, 7);
+			write(LINEA_RES, var_instruccion(7 downto 4), 	right, 7);
+			write(LINEA_RES, var_instruccion(3 downto 0), 	right, 7);
+			
+
+			writeline(ARCH_RES,LINEA_RES);-- escribe la linea en el archivo
+			
+		end loop;
+		file_close(ARCH_VEC);  -- cierra el archivo
+		file_close(ARCH_RES);  -- cierra el archivo
+
+      wait;
 --      wait for <clock>_period*10;
 
       -- insert stimulus here 

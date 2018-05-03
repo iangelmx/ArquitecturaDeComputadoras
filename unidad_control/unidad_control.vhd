4bd2c9dce2c097e259b984245a82149164e8c586
Angel N Ramírez <iangelmx@hotmail.com>
2018-05-03 18:44:11 -0500

    Se corrige Unidad de Control Practica08 Contador de bits

--- unidad_control/unidad_control.vhd
+++ unidad_control/unidad_control.vhd
@@ -1,33 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    18:52:55 05/02/2018 
--- Design Name: 
--- Module Name:    unidad_control - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity unidad_control is
     Port ( ini : in  STD_LOGIC;
@@ -91,7 +64,7 @@ begin
 				if(z = '1') then
 					estado_sig<=E2;
 				else
-					if(a0 = '0')then
+					if(a0 = '1')then
 						lb_a <= '0';
 						eb_a <= '1';
 					end if;

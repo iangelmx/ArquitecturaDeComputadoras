a52116abb1dcf63f03fed0478f490b75409404af
Angel N Ramírez <iangelmx@hotmail.com>
2018-06-05 17:07:53 -0500

    Se termina práctica 10 De unidad de Control
    No se quita el XNOR del componente de nivel

--- PROC_UNID_CONTROL/TB_UnidadDeControl.vhd
+++ PROC_UNID_CONTROL/TB_UnidadDeControl.vhd
@@ -67,6 +67,107 @@ BEGIN
    -- Stimulus process
 	
 	
+--------	-- SE INSERTA TB YAYO ----------------------
+--------	
+--------	 -- Stimulus process
+--------   stim_proc: process
+--------	file ARCH_RES : TEXT;																					
+--------	variable LINEA_RES : line;
+--------	VARIABLE var_microins : STD_LOGIC_VECTOR(19 DOWNTO 0);
+--------	VARIABLE var_nivel : STRING(1 TO 4);
+--------	
+--------	file ARCH_VEC : TEXT;
+--------	variable LINEA_VEC : line;
+--------	VARIABLE var_opcode : STD_LOGIC_VECTOR(4 DOWNTO 0);
+--------	VARIABLE var_funcode : STD_LOGIC_VECTOR(3 DOWNTO 0);
+--------	VARIABLE var_flags : STD_LOGIC_VECTOR(3 DOWNTO 0);
+--------	VARIABLE var_clr : STD_LOGIC;
+--------	VARIABLE var_lf : STD_LOGIC;
+--------	VARIABLE CADENA : STRING(1 TO 16);
+--------   begin		
+--------		file_open(ARCH_VEC, "IN.TXT", READ_MODE); 	
+--------		file_open(ARCH_RES, "OUT.TXT", WRITE_MODE); 	
+--------
+--------		CADENA := " OP-CODE        ";
+--------		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);
+--------		CADENA := " FUN-CODE       ";
+--------		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);
+--------		CADENA := " BANDERAS       ";
+--------		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);
+--------		CADENA := " CLR            ";
+--------		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);
+--------		CADENA := " LF             ";
+--------		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);
+--------		CADENA := " MICINSTRUCCION ";
+--------		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);
+--------		CADENA := " NIVEL          ";
+--------		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);
+--------		writeline(ARCH_RES,LINEA_RES);-- escribe la linea en el archivo
+--------
+--------		WAIT FOR 10 NS;
+--------		FOR I IN 0 TO 50 LOOP
+--------			readline(ARCH_VEC,LINEA_VEC); -- lee una linea completa
+--------
+--------			read(LINEA_VEC, var_opcode);
+--------			op_code <= var_opcode;
+--------			read(LINEA_VEC, var_funcode);
+--------			func_code <= var_funcode;
+--------			read(LINEA_VEC, var_flags);
+--------			banderas <= var_flags;
+--------			read(LINEA_VEC, var_clr);
+--------			clr <= var_clr;
+--------			read(LINEA_VEC, var_lf);
+--------			lf <= var_lf;
+--------			
+--------			--WAIT UNTIL RISING_EDGE(CLK);	--ESPERO AL FLANCO DE SUBIDA 
+--------
+--------			var_microins := microinstruccion;
+--------			--Las que NO tienen var, reemplazar por las de la entidad.
+--------			IF nivel_out = '1' THEN
+--------				var_nivel := "ALTO";	
+--------			ELSE
+--------				var_nivel := "BAJO";
+--------			END IF;
+--------			
+--------			write(LINEA_RES, var_opcode, left, 16);
+--------			write(LINEA_RES, var_funcode, 	left, 16);
+--------			write(LINEA_RES, var_flags, 	left, 16);
+--------			write(LINEA_RES, var_clr, 	left, 16);
+--------			write(LINEA_RES, var_lf, 	left, 16);
+--------			write(LINEA_RES, var_microins, 	left, 16);
+--------			write(LINEA_RES, var_nivel, 	right, 16);
+--------			
+--------			writeline(ARCH_RES,LINEA_RES);-- escribe la linea en el archivo
+--------			
+--------			WAIT UNTIL FALLING_EDGE(CLK);	--ESPERO AL FLANCO DE BAJADA
+--------			
+--------			IF nivel_out = '1' THEN
+--------				var_nivel := "ALTO";	
+--------			ELSE
+--------				var_nivel := "BAJO";
+--------			END IF;
+--------			
+--------			write(LINEA_RES, var_opcode, left, 16);
+--------			write(LINEA_RES, var_funcode, 	left, 16);
+--------			write(LINEA_RES, var_flags, 	left, 16);
+--------			write(LINEA_RES, var_clr, 	left, 16);
+--------			write(LINEA_RES, var_lf, 	left, 16);
+--------			write(LINEA_RES, var_microins, 	left, 16);
+--------			write(LINEA_RES, var_nivel, 	right, 16);
+--------
+--------			writeline(ARCH_RES,LINEA_RES);-- escribe la linea en el archivo
+--------			
+--------			WAIT UNTIL RISING_EDGE(CLK);	--ESPERO AL FLANCO DE SUBIDA 
+--------			
+--------		end loop;
+--------		file_close(ARCH_VEC);  -- cierra el archivo
+--------		file_close(ARCH_RES);  -- cierra el archivo
+--------
+--------      wait;
+--------   end process;
+--------	
+--------	-- FINALIZA TB YAYO ----------------------------------------
+
 	-- SE INSERTA TB YAYO ----------------------
 	
 	 -- Stimulus process
@@ -119,33 +220,39 @@ BEGIN
 			read(LINEA_VEC, var_lf);
 			lf <= var_lf;
 			
-			--WAIT UNTIL RISING_EDGE(CLK);	--ESPERO AL FLANCO DE SUBIDA 
-
+--			WAIT UNTIL RISING_EDGE(CLK);	--ESPERO AL FLANCO DE SUBIDA 
+--
 			var_microins := microinstruccion;
 			--Las que NO tienen var, reemplazar por las de la entidad.
-			IF nivel_out = '1' THEN
-				var_nivel := "ALTO";	
-			ELSE
-				var_nivel := "BAJO";
-			END IF;
+--			IF nivel_out = '1' THEN
+--				var_nivel := "ALTO";	
+--			ELSE
+--				var_nivel := "BAJO";
+--			END IF;
 			
-			write(LINEA_RES, var_opcode, left, 16);
-			write(LINEA_RES, var_funcode, 	left, 16);
-			write(LINEA_RES, var_flags, 	left, 16);
-			write(LINEA_RES, var_clr, 	left, 16);
-			write(LINEA_RES, var_lf, 	left, 16);
+--			write(LINEA_RES, var_opcode, left, 16);
+--			write(LINEA_RES, var_funcode, 	left, 16);
+--			write(LINEA_RES, var_flags, 	left, 16);
+--			write(LINEA_RES, var_clr, 	left, 16);
+--			write(LINEA_RES, var_lf, 	left, 16);
+			write(LINEA_RES, "                ", left, 16);
+			write(LINEA_RES, "                ", 	left, 16);
+			write(LINEA_RES, "                ", 	left, 16);
+			write(LINEA_RES, "                ", 	left, 16);
+			write(LINEA_RES, "                ", 	left, 16);
 			write(LINEA_RES, var_microins, 	left, 16);
-			write(LINEA_RES, var_nivel, 	right, 16);
+			write(LINEA_RES, "BAJO", 	right, 16);
 			
 			writeline(ARCH_RES,LINEA_RES);-- escribe la linea en el archivo
 			
 			WAIT UNTIL FALLING_EDGE(CLK);	--ESPERO AL FLANCO DE BAJADA
 			
-			IF nivel_out = '1' THEN
-				var_nivel := "ALTO";	
-			ELSE
-				var_nivel := "BAJO";
-			END IF;
+			var_microins := microinstruccion;
+--			IF nivel_out = '1' THEN
+--				var_nivel := "ALTO";	
+--			ELSE
+--				var_nivel := "BAJO";
+--			END IF;
 			
 			write(LINEA_RES, var_opcode, left, 16);
 			write(LINEA_RES, var_funcode, 	left, 16);
@@ -153,7 +260,7 @@ BEGIN
 			write(LINEA_RES, var_clr, 	left, 16);
 			write(LINEA_RES, var_lf, 	left, 16);
 			write(LINEA_RES, var_microins, 	left, 16);
-			write(LINEA_RES, var_nivel, 	right, 16);
+			write(LINEA_RES, "ALTO", 	right, 16);
 
 			writeline(ARCH_RES,LINEA_RES);-- escribe la linea en el archivo
 			
@@ -167,6 +274,7 @@ BEGIN
    end process;
 	
 	-- FINALIZA TB YAYO ----------------------------------------
+
 	
 	
 --   stim_proc: process

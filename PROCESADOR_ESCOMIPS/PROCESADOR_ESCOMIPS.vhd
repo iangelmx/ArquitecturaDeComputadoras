dd5b9733a17182ac3fc4611dc37ad5cadcddd192
Angel N Ramírez <iangelmx@hotmail.com>
2018-06-06 16:21:40 -0500

    Se crea archivo PADRE del procesador #32

---
+++ PROCESADOR_ESCOMIPS/PROCESADOR_ESCOMIPS.vhd
@@ -0,0 +1,19 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity PROCESADOR_ESCOMIPS is
+	Port(
+		clk, clr : in STD_LOGIC;
+		read_data2 : out STD_LOGIC_VECTOR (15 downto 0)
+	);
+end PROCESADOR_ESCOMIPS;
+
+architecture Behavioral of PROCESADOR_ESCOMIPS is
+
+begin
+
+
+end Behavioral;
+

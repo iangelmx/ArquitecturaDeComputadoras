2f51c3f5d6009d9c95808ef181bd26b48f8f2c04
Angel N Ramírez <iangelmx@hotmail.com>
2018-06-14 20:57:11 -0500

    Se hace primera versión del código de la práctica 12
    close #45
    
    close #43
    
    close #42

--- MEMORIA_PROGRAMA/MEMORIA_PROGRAMA.vhd
+++ MEMORIA_PROGRAMA/MEMORIA_PROGRAMA.vhd
@@ -23,23 +23,39 @@ architecture Behavioral of MEMORIA_PROGRAMA is
   constant R2		: std_logic_vector(3 downto 0)	:= x"2";
   constant R3		: std_logic_vector(3 downto 0)	:= x"3";
   constant R4		: std_logic_vector(3 downto 0)	:= x"4";
-  constant R4		: std_logic_vector(3 downto 0)	:= x"5";
-  constant R4		: std_logic_vector(3 downto 0)	:= x"6";
-  constant R4		: std_logic_vector(3 downto 0)	:= x"7";
-  constant R4		: std_logic_vector(3 downto 0)	:= x"8";
-  constant S_U		: std_logic_vector(3 downto 0)	:= x"0";
+  constant R5		: std_logic_vector(3 downto 0)	:= x"5";
+  constant R6		: std_logic_vector(3 downto 0)	:= x"6";
+  constant R7		: std_logic_vector(3 downto 0)	:= x"7";
+  constant R8		: std_logic_vector(3 downto 0)	:= x"8";
+  constant R9		: std_logic_vector(3 downto 0)	:= x"9";
+  constant R10		: std_logic_vector(3 downto 0)	:= x"A";
+  
+  constant R11		: std_logic_vector(3 downto 0)	:= x"B";
+  constant SU		: std_logic_vector(3 downto 0)	:= x"0";
   
   constant LI		: std_logic_vector(4 downto 0)	:= "00001";		--1
   constant SWI		: std_logic_vector(4 downto 0)	:= "00011";		--3
   constant BGTI	: std_logic_vector(4 downto 0)	:= "10001";		--17
+  constant BLTI	: std_logic_vector(4 downto 0)	:= "01111";		--15
   constant ADDI	: std_logic_vector(4 downto 0)	:= "00101";		--5
   constant B		: std_logic_vector(4 downto 0)	:= "10011";		--19
   constant CALL	: std_logic_vector(4 downto 0)	:= "10100";		--20
+  constant RET		: std_logic_vector(4 downto 0)	:= "10101";		--21
   constant LW		: std_logic_vector(4 downto 0)	:= "10111";		--23
   constant SUBI	: std_logic_vector(4 downto 0)	:= "00110";		--6
   constant SW		: std_logic_vector(4 downto 0)	:= "00100";		--4
   
+  constant INICIALIZA	: std_logic_vector(15 downto 0)	:= "0000000000000100";
+  constant BURBUJA		: std_logic_vector(15 downto 0)	:= "0000000000110110";
+  constant DESPLIEGUE	: std_logic_vector(15 downto 0)	:= "0000000001000011";
   
+  constant VACIO 			: std_logic_vector(24 downto 0) := "0000000000000000000000000";
+  
+  constant DOS_NEG	: std_logic_vector(11 downto 0) := "111111111110";	-- -2
+  constant DOCE_NEG	: std_logic_vector(11 downto 0) := "111111110100"; -- -12
+  constant NUEV_NEG	: std_logic_vector(11 downto 0) := "111111110111"; -- -9
+  constant CERO_12	: std_logic_vector(11 downto 0) := x"000";
+   
 
 --  constant ADD	: std_logic_vector(4 downto 0)	:= "00000";
   
@@ -49,79 +65,90 @@ type arreglo is array (0 to ((2**busT)-1)) of std_logic_vector((palabra-1) downt
 
 constant banco: arreglo :=(
 ------ INSTRUCCIONES DE PRÁCTICA 11
-------                        LI& R0&x"0001",							--"00001 0000 0000 0000 0000 0001",
-------                        LI& R1&x"0007",							--"00001 0001 0000 0000 0000 0111",
-------								ADD& R1& R1& R0&"0000"&"0000",		--"00000 0001 0001 0000 0000 0000",
-------                        SWI& R1&x"0005",						--"00011 0001 0000 0000 0000 0101",
+------                        LI	& R0&x"0001",							--"00001 0000 0000 0000 0000 0001",
+------                        LI	& R1&x"0007",							--"00001 0001 0000 0000 0000 0111",
+------								ADD	& R1	& R1	& R0&"0000"&"0000",		--"00000 0001 0001 0000 0000 0000",
+------                        SWI	& R1&x"0005",						--"00011 0001 0000 0000 0000 0101",
 ------                        B&"0000"&x"0002",						--"10011 0000 0000 0000 0000 0010",
-						LI  & R0 & x"0005",
-						SWI & R0 & x"0001",
-						LI  & R0 & x"0007",
-						SWI & R0 & x"0002",
-						LI  & R0 & x"0009",
-						SWI & R0 & x"0003",
-						LI  & R0 & x"000" & ""--11
-						SWI & R0 & 4
-						LI  & R0 & 13
-						SWI & R0 & 5
-						LI  & R0 & 15
-						SWI & R0 & 6
-						LI  & R0 & 17
-						SWI & R0 & 7
-						LI  & R0 & 19
-						SWI & R0 & 8
-						LI  & R0 & 1
-						SWI & R0 & 9
-						LI  & R0 & 3
-						SWI & R0 & 10
-						LI  & R0 & 20
-						SWI & R0 & 11
-						LI  & R0 & 18
-						SWI & R0 & 12
-						LI  & R0 & 16
-						SWI & R0 & 13
-						LI  & R0 & 14
-						SWI & R0 & 14
-						LI  & R0 & 12
-						SWI & R0 & 15
-						LI  & R0 & 10
-						SWI & R0 & 16
-						LI  & R0 & 8
-						SWI & R0 & 17
-						LI  & R0 & 6
-						SWI & R0 & 18
-						LI  & R0 & 4
-						SWI & R0 & 19
-						LI  & R0 & 2
-						SWI & R0 & 20
-						LI  & R1 & 20
-						LI  & R2 & 20
-						LI  & R3 & 0
-						LI  & R4 & 0
-						BGTI R4 & R2 & 6
-						BGTI R4 & R1 & 2
-						ADDI R1 &  R4 &  20
-						B #46
-						LW R5 &  0(R1)
-						SUBI R1 &  R1 &  1
-						B #46
-						BGTI R4 & R1 & 3
-						SUBI R2 & R2 & 1
-						ADDI R1 & R4 & 20
-						B #45
-						LW R6 &  0(R1)
-						SUBI R7 &  R6 &  1
-						LW R8 &  0(R7)
-						BGTI R8 &  R6 & 3
-						SUBI R1 &  R1 &  1
-						B #52
-						ADDI R3 &  R8 &  0
-						SW R6 &  0(R7)
-						SW R3 &  0(R1)
-						B#60
+							CALL & SU & INICIALIZA,
+						-- 10100 0000 0000000000000100
+							CALL & SU & BURBUJA,
+							CALL & SU & DESPLIEGUE,
+			VACIO,
+							LI 	& R0 & x"0005",
+							SWI 	& R0 & x"0001",
+							LI 	& R0 & x"0007",
+							SWI 	& R0 & x"0002",
+							LI 	& R0 & x"0009",
+							SWI 	& R0 & x"0003",
+							LI 	& R0 & x"000B",  --11
+							SWI 	& R0 & x"0004",
+							LI 	& R0 & x"000D",		--13
+							SWI 	& R0 & x"0005",
+							LI 	& R0 & x"000F",		--15
+							SWI 	& R0 & x"0006",
+							LI 	& R0 & x"0011",		--17
+							SWI 	& R0 & x"0007",
+							LI 	& R0 & x"0013",		--19
+							SWI 	& R0 & x"0008",
+							LI 	& R0 & x"0001",
+							SWI 	& R0 & x"0009",
+							LI 	& R0 & x"0003",
+							SWI 	& R0 & x"000A",		--10
+							LI 	& R0 & x"0014",		--20
+							SWI 	& R0 & x"000B",			--11
+							LI 	& R0 & x"0012",			--18
+							SWI 	& R0 & x"000C",		--12
+							LI 	& R0 & x"0010",		--16
+							SWI 	& R0 & x"000D",			--13
+							LI 	& R0 & x"000E",			--14
+							SWI 	& R0 & x"000E",			--14
+							LI 	& R0 & x"0018",				--24
+							SWI 	& R0 & x"000F",			--15
+							LI 	& R0 & x"000A", 			-- 10,------------------
 
 
-								others => (others => '0') );
+						SWI & R0 & x"0010",
+						LI & R0 & x"0008",
+						SWI & R0 & x"0011",
+						LI & R0 & x"0006",
+						SWI & R0 & x"0012",
+						LI & R0 & x"0004",
+						SWI & R0 & x"0013",
+						LI & R0 & x"0002",
+						SWI & R0 & x"0014",
+						LI & R1 & x"0014",
+						LI & R2 & x"0014",
+						LI & R3 & x"0000",
+						LI & R4 & x"0000",
+						LI & R10 & x"0013",
+						LI & R11 & x"0000",
+						RET & SU & SU & SU & SU & SU,
+				VACIO,
+						LW 	& R5 & R1 & CERO_12,
+						LW 	& R6 & R10 & CERO_12,
+						BLTI 	& R6 & R5 & x"006",
+						LW 	& R3 & R10 & CERO_12,
+						LW 	& R7 & R1 & CERO_12,
+						SW 	& R7 & R10 & CERO_12,
+						SW 	& R3 & R1 & CERO_12,
+						SUBI 	& R10 & R10 & x"001",
+						SUBI	& R1 & R1 & x"001",
+
+
+
+							BGTI	& R11	& R1 & NUEV_NEG,			-- -9 -> C,  DE AQUÍ PARA ARRIBA ES DE YAYO
+							SUBI	& R2 	& R2 & x"001",
+							LI 	& R1 	& x"0014",					--20
+							BGTI	& R11	& R2 & DOCE_NEG,			-- -12 -> C,		
+							RET 	& SU 	& SU & SU & SU & SU,
+		VACIO,
+							LW 	& R9 	& R1 & CERO_12,
+							SUBI 	& R1 	& R1 & x"001",
+							BGTI 	& R11	& R1 & DOS_NEG,			-- -2 -> D,
+							LI 	& R1 	& x"0014",					--20
+							B & SU & x"0043",
+							others => (others => '0') );
 begin
 
 	instruccion <= banco(conv_integer(PC));
--- PROCESADOR_ESCOMIPS/PROCESADOR_ESCOMIPS.vhd
+++ PROCESADOR_ESCOMIPS/PROCESADOR_ESCOMIPS.vhd
@@ -9,7 +9,8 @@ use work.COMPONENTES.all;
 entity PROCESADOR_ESCOMIPS is
 	Port(
 		clk, clr : in STD_LOGIC;
-		read_data2 : out STD_LOGIC_VECTOR (7 downto 0);
+--		read_data2 : out STD_LOGIC_VECTOR (7 downto 0);
+		salida : out STD_LOGIC_VECTOR(15 downto 0);
 --		read_data1 : out STD_LOGIC_VECTOR (15 downto 0);
 		PC_fin_out : out STD_LOGIC_VECTOR(7 downto 0)
 --		instruccion_out : out STD_LOGIC_VECTOR(24 downto 0);
@@ -121,7 +122,8 @@ begin
 	SR : multiplexor port map(opc0=>memd_o,opc1=>alu_o,selector=>msr,sal=>sr_out);
 -------------------- FIN BLOQUES YAYO--------------------------------------------------------------------
 	
-	read_data2 <= rdata2_aux(7 downto 0);
+	--read_data2 <= rdata2_aux(7 downto 0);
+	salida <= memd_o;
 	PC_fin_out <= PCout_aux(7 downto 0);
 	
 
